49219aaeae59b2ace05e7c8af5e01eea87e2ea7a
mkobawallets <lucymwithalii@gmail.com>
2026-05-21 23:28:06 +0300

    Create db.js to configure database connection

---
+++ src/db.js
@@ -0,0 +1,8 @@
+// src/db.js
+const knex   = require('knex')
+const config = require('../knexfile')
+
+const env = process.env.NODE_ENV || 'development'
+const db  = knex(config[env])
+
+module.exports = db
